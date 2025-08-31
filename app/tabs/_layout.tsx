1a21fb374b0bc7f1910817a633072b00029ad275
krrish-anand11 <krrish.anand11@gmail.com>
2025-08-30 18:23:47 -0700

    Create _layout.tsx

---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,74 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { PetProvider } from "@/providers/PetProvider";
+import { DebateProvider } from "@/providers/DebateProvider";
+
+SplashScreen.preventAutoHideAsync();
+
+const queryClient = new QueryClient();
+
+function RootLayoutNav() {
+  return (
+    <Stack screenOptions={{ headerBackTitle: "Back" }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
+
+export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <PetProvider>
+          <DebateProvider>
+            <RootLayoutNav />
+          </DebateProvider>
+        </PetProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
+  );
+}
